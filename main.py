e2f575f50665e98cc1c9052ffb409e967dbd21db
EriottosanMC <94922833+EPats@users.noreply.github.com>
2023-08-27 18:19:25 +0100

    Tree implementation, requires input to have spaces between each "token"

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import re
 import json
 
 NUMBER_PATTERN = re.compile(r'[0-9]*?.\.{0,1}[0-9e]+')
-
+OPERATOR_PRECEDENCE = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3, 'sqrt': 3}
 
 def add(x, y):
     return x + y
@@ -36,6 +36,83 @@ def parse_input(ip):
     numbers = NUMBER_PATTERN.match(ip)
 
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+# Helper function to evaluate an expression tree
+def evaluate_tree(node):
+    if node is None:
+        return 0
+
+    # If leaf node, return its value
+    if node.left is None and node.right is None:
+        return float(node.value)
+
+    left_value = evaluate_tree(node.left)
+    right_value = evaluate_tree(node.right)
+
+    # Perform the operation based on the operator in the node's value
+    if node.value == '+':
+        return left_value + right_value
+    if node.value == '-':
+        return left_value - right_value
+    if node.value == '*':
+        return left_value * right_value
+    if node.value == '/':
+        if right_value == 0:
+            return "Division by zero."
+        return left_value / right_value
+    if node.value == '^':
+        return left_value ** right_value
+    if node.value == 'sqrt':
+        if left_value < 0:
+            return "Square root of negative number."
+        return left_value ** 0.5
+
+
+# Function to create an expression tree from a tokenized expression (infix notation)
+def build_expression_tree(tokens):
+    stack = []
+    output = []
+
+    for token in tokens:
+        if token in OPERATOR_PRECEDENCE:
+            while (stack and stack[-1] in OPERATOR_PRECEDENCE and
+                   OPERATOR_PRECEDENCE[token] <= OPERATOR_PRECEDENCE[stack[-1]]):
+                output.append(stack.pop())
+            stack.append(token)
+        elif token == '(':
+            stack.append(token)
+        elif token == ')':
+            while stack and stack[-1] != '(':
+                output.append(stack.pop())
+            stack.pop()
+        else:  # Operand
+            output.append(token)
+
+    while stack:
+        output.append(stack.pop())
+
+        # Build the expression tree from the output (postfix notation)
+    for token in output:
+        if token not in OPERATOR_PRECEDENCE:
+            stack.append(Node(token))
+        else:
+            node = Node(token)
+            if token == 'sqrt':  # Unary operator
+                node.left = stack.pop()
+            else:  # Binary operator
+                node.right = stack.pop()
+                node.left = stack.pop()
+            stack.append(node)
+
+    return stack[0] if stack else None
+
+
 test_operations = {
     'add': add(5, 3),
     'subtract': subtract(5, 3),
@@ -48,3 +125,9 @@ test_operations = {
 }
 
 print(json.dumps(test_operations, indent=4))
+# Test the expression tree building and evaluation
+expression = input()
+tokens = expression.split()
+root = build_expression_tree(tokens)
+result = evaluate_tree(root)
+print(result)
