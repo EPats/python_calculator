9ca5ccb48b2ed65bf10ceaf6d18b09c8cdb6cd69
EriottosanMC <94922833+EPats@users.noreply.github.com>
2023-08-27 18:45:35 +0100

    Better testing setup, now tells us if we fail (WE DID!)

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import re
 import json
 
-TOKEN_PATTERN = re.compile(r'(\b\w+[\.]?\w*\b|[+\-*/^()])')
+TOKEN_PATTERN = re.compile(r'(-?\b\w+[\.]?\w*\b|[+\-*/^()])')
 OPERATOR_PRECEDENCE = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3, 'sqrt': 3}
+DIVIDE_ZERO_ERROR_STRING = 'Division by zero is not allowed.'
+NEGATIVE_ROOT_ERROR_STRING = 'Sqrt of negative number not allowed.'
 
 def add(x, y):
     return x + y
@@ -18,7 +20,7 @@ def multiply(x, y):
 
 def divide(x, y):
     if y == 0:
-        return "Division by zero is not allowed."
+        return DIVIDE_ZERO_ERROR_STRING
     return x / y
 
 
@@ -28,7 +30,7 @@ def power(x, y):
 
 def root(x):
     if x < 0:
-        return "Square root of negative number is not allowed."
+        return NEGATIVE_ROOT_ERROR_STRING
     return power(x, 0.5)
 
 
@@ -64,13 +66,13 @@ def evaluate_tree(node):
         return left_value * right_value
     if node.value == '/':
         if right_value == 0:
-            return "Division by zero."
+            return DIVIDE_ZERO_ERROR_STRING
         return left_value / right_value
     if node.value == '^':
         return left_value ** right_value
     if node.value == 'sqrt':
         if left_value < 0:
-            return "Square root of negative number."
+            return NEGATIVE_ROOT_ERROR_STRING
         return left_value ** 0.5
 
 
@@ -133,19 +135,35 @@ test_operations = {
 print(json.dumps(test_operations, indent=4))
 # Test the expression tree building and evaluation
 test_expressions = [
-    '5+3',
-    '(3+4)*5',
-    '3+4*5',
-    '4/2',
-    '8/9',
-    '9/0',
-    '(2+5)/(3-3)',
-    '100/(3+2)',
-    '3*(4+5)',
-    'sqrt(25)',
-    'sqrt(24)',
-    '3^2',
-    '3.2 * 2.5'
+    ('5+3', 8),
+    ('(3+4)*5', 35),
+    ('3+4*5', 23),
+    ('4/2', 2),
+    ('8/9', 8/9),
+    ('9/0', DIVIDE_ZERO_ERROR_STRING),
+    ('(2+5)/(3-3)', DIVIDE_ZERO_ERROR_STRING),
+    ('100/(3+2)', 20),
+    ('3*(4+5)', 27),
+    ('sqrt(25)', 5),
+    ('sqrt(24)', root(24)),
+    ('3^2', 9),
+    ('3.2 * 2.5', 8),
+    ('3+-2', 1),
+    ('(2*-4)', -8),
+    ('sqrt(-25)', NEGATIVE_ROOT_ERROR_STRING),
+    ('5 + -3', 2),
+    ('(-3+2)*(2+4)', -6),
+    ('(-3)^2', 9),
+    ('5', 5),
+    ('-2', -2)
 ]
-test_expressions_results = {expr: parse_and_eval(expr) for expr in test_expressions}
+test_expressions_results = [{'expression': expr, 'passed': parse_and_eval(expr) == answer, 'expected': answer,
+                             'received': parse_and_eval(expr)} for expr, answer in test_expressions]
 print(json.dumps(test_expressions_results, indent=4))
+print('\n\n')
+all_correct = all([expr['passed'] for expr in test_expressions_results])
+if all_correct:
+    print('All tests passed')
+else:
+    print(f'{len(test_expressions_results) - sum([1 if expr["passed"] else 0 for expr in test_expressions_results])} tests failed')
+    print(json.dumps([test for test in test_expressions_results if not test['passed']], indent=4))
